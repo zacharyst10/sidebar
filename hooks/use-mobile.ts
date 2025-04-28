b71db99d383867e3f8b3d10da5fa1175138f90ec
Zach Stout <stout.zachary@gmail.com>
2025-04-28 11:26:05 -0600

    hello?

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,7 +1,6 @@
 import * as React from "react";
 
-const MOBILE_BREAKPOINT = 1000;
-
+const MOBILE_BREAKPOINT = 1200;
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
     undefined
